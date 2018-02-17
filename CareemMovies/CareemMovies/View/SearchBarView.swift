44bb0159f760eb6c884c0b58509c8d863b672e21
Siarhei Barmotska <siarhei.barmotska@glispamedia.com>
2018-02-17 13:15:54 +0100

    added UISearchBar as header of movies list

---
+++ CareemMovies/CareemMovies/View/SearchBarView.swift
@@ -0,0 +1,15 @@
+//
+//  SearchBarView.swift
+//  CareemMovies
+//
+//  Created by Siarhei Barmotska on 17.02.18.
+//  Copyright © 2018 Siarhei Barmotska. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarView: UICollectionReusableView {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    static let identifier = "SearchBarView"
+}
--- CareemMovies/CareemMovies/ViewController/MoviesListViewController.swift
+++ CareemMovies/CareemMovies/ViewController/MoviesListViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class MoviesListViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
+  
     private var arr = [Int](0...20)
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
 }
+
 
 extension MoviesListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -31,9 +34,38 @@ extension MoviesListViewController: UICollectionViewDataSource {
         return UICollectionViewCell()
     }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        switch kind {
+        case UICollectionElementKindSectionHeader:
+            return collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: SearchBarView.identifier, for: indexPath)
+            
+        default:
+            break
+        }
+        
+        return UICollectionReusableView()
+    }
+    
 }
 
+
 extension MoviesListViewController: UICollectionViewDelegate {
+    
+}
+
+
+extension MoviesListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+            collectionView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            collectionView.reloadData()
+        }
+    }
     
 }
 
